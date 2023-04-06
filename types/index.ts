bba59c7e3b91ab013ab83a2eb175d59778353022
Tahir Taim <taimtahir654@gmail.com>
2023-04-07 01:03:47 +0600

    updated

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+export interface MovieInterface {
+  id: string;
+  title: string;
+  description: string;
+  thumbnailUrl: string;
+  videoUrl: string;
+  duration: string;
+  genre: string;
+  year: string;
+  trailerUrl: string;
+}
